319d35833ea2472833ecd18bf4dae12571390414
Fabio Battaglia <hkzlabnet@gmail.com>
2014-04-20 22:51:59 +0200

    Add basic I/O code for n8vem console

--- src/io/boards/n8vem_conio.c
+++ src/io/boards/n8vem_conio.c
@@ -5,5 +5,16 @@ static __sfr __at 0x00 PropCIO_STAT;
 static __sfr __at 0x01 PropCIO_DATA;
 
 void n8vem_conio_init(void) {
+	// Nothing to do...
 	return;
+}
+
+uint8_t n8vem_conio_getch(void) {
+	while(!(PropCIO_STAT & 0x02)); // Loop until we are ready to get the data...
+	return PropCIO_DATA;
+}
+
+void n8vem_conio_putch(uint8_t ch) {
+	while(!(PropCIO_STAT & 0x04)); // Loop until we are ready to send the data...
+	PropCIO_DATA = ch; // Send the data
 }
--- src/io/boards/n8vem_conio.h
+++ src/io/boards/n8vem_conio.h
@@ -5,4 +5,7 @@
 
 void n8vem_conio_init(void);
 
+uint8_t n8vem_conio_getch(void);
+void n8vem_conio_putch(uint8_t ch);
+
 #endif /* _N8VEMCONIO_HEADER_ */
