e474b7c8c35cde2d0ac3f2249b7215571b6a973c
Fabio Battaglia <hkzlabnet@gmail.com>
2014-05-25 17:46:39 +0200

    Adding utility code for xmodem

--- src/io/xmodem.c
+++ src/io/xmodem.c
@@ -6,18 +6,34 @@
 #include "boards/n8vem_serio.h"
 
 #define SOH  0x01
-#define STX  0x02
 #define EOT  0x04
 #define ACK  0x06
 #define NAK  0x15
 #define CAN  0x18
-#define CTRLZ 0x1A
 
+#define CTRLZ 0x1A
 #define MAXRETRANS 25
 
 
+static void flush(void);
+static uint8_t checksum(uint8_t *buf);
 
 uint8_t xmodem_receive(uint8_t* dest) {
 	return 0xFF;
 }
 
+static void flush(void) {
+	uint8_t val = 1;
+
+	while(val)
+		n8vem_serio_getch_nb(&val);
+}
+
+static uint8_t checksum(uint8_t *buf) {
+	uint8_t val = 0, idx;
+
+	for (idx = 0; idx < 128; idx++)
+		val += buf[idx];
+
+	return val;
+}
